7ddaaabf351e675c12e7aaf79d26c628b5400fd6
VerDel Cargal <verdel@cargal.net>
2022-05-04 16:08:53 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by VerDel on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by VerDel on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
